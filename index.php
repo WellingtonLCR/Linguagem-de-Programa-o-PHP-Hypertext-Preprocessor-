32f1d6b8264ca4aa6410f434dab286c6cb475c12
WellingtonLCR <dabliojau@gmail.com>
2025-05-12 01:10:14 -0300

    Embutindo blocos blocos PHP em paginas HTML

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-teste
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Embutindo blocos blocos PHP em paginas HTML</title>
+    </head>
+
+    <body>
+        <?php echo 'Utilizando a tag padrão';?>
+        <br>
+            <?php echo 'Utilizando a tag impressão'?>
+        <br>
+            <?php echo'Utilizando a tag curta';?>
+    </body>
+</html>
