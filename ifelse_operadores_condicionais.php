07f94c742900e50cc1dd8f922d2dc4e705917d77
WellingtonLCR <dabliojau@gmail.com>
2025-05-13 11:49:41 -0300

    Praticando Operdores Lógicos

--- ifelse_operadores_condicionais.php
+++ ifelse_operadores_condicionais.php
@@ -2,31 +2,32 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Praticando Operdores de Comparação - Parte 1</title>
+        <title>Praticando Operdores de Comparação</title>
     </head>
 
     <body>
         <?php
-        //1
-        if (3 == 2) {
-            echo ' Verdadeiro';
-        } else {
-            echo 'Falso';
-        }
 
-        echo '<br/><br/>';
+            //1
+            if (3 == 2) {
+                echo ' Verdadeiro';
+            } else {
+                echo 'Falso';
+            }
 
-        //2
-        if (3 === '2') {
-            echo 'Verdadeiro';
-        } else {
-            echo 'Falso';
-        }
+            echo '<br/><br/>';
 
-        echo '<br/><br/>';
+            //2
+            if (3 === '2') {
+                echo 'Verdadeiro';
+            } else {
+                echo 'Falso';
+            }
 
-        //3
-        if (5 != 5) {
+            echo '<br/><br/>';
+
+            //3
+            if (5 != 5) {
                 echo 'Verdadeiro';
             } else {
                 echo 'Falso';
@@ -38,58 +39,66 @@
                 echo ' - Falso';
             }
 
-        echo '<br/><br/>';
+            echo '<br/><br/>';
 
-        //4
-        if (5 !== 5) {
-            echo 'Verdadeiro';
-        } else {
-            echo 'Falso';
-        }
+            //4
+            if (5 !== 5) {
+                echo 'Verdadeiro';
+            } else {
+                echo 'Falso';
+            }
 
-        echo '<br/><br/>';
+            echo '<br/><br/>';
 
-        //5
-        if (18 < 18) {
-            echo 'Verdadeiro';
-        } else {
-            echo 'Falso';
-        }
+            //5
+            if (18 < 18) {
+                echo 'Verdadeiro';
+            } else {
+                echo 'Falso';
+            }
 
-        echo '<br/><br/>';
+            echo '<br/><br/>';
 
-        //6
-        if (18 > 18) {
-            echo 'Verdadeiro';
-        } else {
-            echo 'Falso';
-        }
+            //6
+            if (18 > 18) {
+                echo 'Verdadeiro';
+            } else {
+                echo 'Falso';
+            }
 
-        echo '<br/><br/>';
+            echo '<br/><br/>';
 
-        //7
-        if (25 <= 15) {
-            echo 'Verdadeiro';
-        } else {
-            echo 'Falso';
-        }
+            //7
+            if (25 <= 15) {
+                echo 'Verdadeiro';
+            } else {
+                echo 'Falso';
+            }
 
-        echo '<br/><br/>';
+            echo '<br/><br/>';
 
-        //8
-        if (-18 >= 25) {
-            echo 'Verdadeiro';
-        } else {
-            echo 'Falso';
-        }
+            //8
+            if (-18 >= 25) {
+                echo 'Verdadeiro';
+            } else {
+                echo 'Falso';
+            }
 
-        echo '<br/><br/>';
+            echo '<br/><br/>';
 
-        if (5 != 4) {
-            echo 'Verdadeiro';
-            echo ' - Segunda instrução';
-            echo ' - Terceira instrução';
-        }
+            if (5 != 4) {
+                echo 'Verdadeiro';
+                echo ' - Segunda instrução';
+                echo ' - Terceira instrução';
+            }
+
+            //AND ou ou && - Verifica se todas as condições são verdadeiras.
+
+            //OR ou || - Verifica se pelo menos uma das condições é verdadeira.
+
+            //XOR - Verifica se apenas uma das condições é verdadeira.
+
+            //NOT - Verifica se a condição eh falsa.
         ?>
     </body>
 </html>
---
+++ ifelse_operadores_logicos.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Praticando Operdores Lógicos</title>
+    </head>
+
+    <body>
+        <?php
+        //Operadores Lógicos e Comparação
+        
+        //1.
+        //AND ou ou && - Verifica se todas as condições são verdadeiras.
+        /*
+            Então o operador && retorna verdadeiro se todas expressões forem verdadeiras. 
+                Isso significa que ele verifica se todas as condições são verdadeiras.
+            
+            Verdadeiro && Verdadeiro = Verdadeiro
+            Verdadeiro && Falso = Falso
+            Falso && Verdadeiro = Falso
+            Falso && Falso = Falso
+        */
+        if (5 == 3 && 10 > 3) {
+            echo 'Verdadeiro';
+        } else {
+            echo 'Falso';
+        }
+
+        echo '<br/><br/>';
+
+        //2.
+        //OR ou || - Verifica se pelo menos uma das condições é verdadeira.
+        /*
+            Então o operador || retorna verdadeiro se pelo menos uma expressão for verdadeira.
+            Isso significa que ele verifica se pelo menos uma das condições é verdadeira.
+        
+            Verdadeiro || Verdadeiro = Verdadeiro
+            Verdadeiro || Falso = Verdadeiro
+            Falso || Verdadeiro = Verdadeiro
+            Falso || Falso = Falso
+        */
+        if (3 == 4 || 10 > 15) {
+            echo 'Verdadeiro';
+        } else {
+            echo 'Falso';
+        }
+
+        echo '<br/><br/>';
+
+        //3.
+        //XOR - Verifica se apenas uma das condições é verdadeira.
+        /*
+            Então o operador XOR retorna verdadeiro se uma das expressões for 
+            verdadeira e a outra for falsa ou vice-versa.
+            Isso significa que ele verifica se apenas uma das condições é verdadeira.
+
+            Verdadeiro XOR Verdadeiro = Falso
+            Verdadeiro XOR Falso = Verdadeiro
+            Falso XOR Verdadeiro = Verdadeiro
+            Falso XOR Falso = Falso
+        */
+        if (7 >= 4 XOR 10 < 15) {
+            echo 'Verdadeiro';
+        } else {
+            echo 'Falso';
+        }
+
+        echo '<br/><br/>';
+
+        //4.
+        //NOT - Verifica se a condição eh falsa.
+        /*
+            Então o operador NOT retorna o inverso da expressão. 
+            Ou seja, ele retorna verdadeiro se a expressão for falsa.
+
+            NOT Verdadeiro = Falso
+            NOT Falso = Verdadeiro
+        */
+        if (!('A' == 'A')) {
+            echo 'Verdadeiro';
+        } else {
+            echo 'Falso';
+        }
+
+        //Exemplos de uso
+        //() - Estabelecer precedência
+        
+        echo '<br/><br/>';
+
+        if ((22 == 21 && 3 == 3) || 5 != 5) {
+            echo 'Verdadeiro';
+        } else {
+            echo 'Falso';
+        }
+        ?>
+    </body>
+</html>
