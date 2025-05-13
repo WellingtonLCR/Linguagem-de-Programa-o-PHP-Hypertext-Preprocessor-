fcb11a71d24b46a84718c5b7b4ec64b58f2482c2
WellingtonLCR <dabliojau@gmail.com>
2025-05-13 03:08:01 -0300

    Praticando Operdaores de Comparação

---
+++ praticando_com_operadores_de_comparacao.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Praticando Operdaores de Comparação</title>
+    </head>
+
+    <body>
+        <?php
+            
+        ?>
+    </body>
+</html>
